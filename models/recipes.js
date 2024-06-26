ca46314438b689be69b80246a5405064fe31bad6
Abshir-Ali <abshir.dev@gmail.com>
2024-06-26 18:59:54 -0400

    formatting

--- models/recipes.js
+++ models/recipes.js
@@ -1,17 +1,19 @@
-
-const moment = require('moment')
-const mongoose = require('mongoose');
+const moment = require("moment");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   title: { type: String, required: true },
   ingredients: [{ type: String }],
   instructions: { type: String, required: true },
-  image: {type: String},
-  cookTime: { type: String, default: '0' },
-  createdAt: { type: String, default: () => moment().format('YYYY-MM-DD HH:mm:ss') }
+  image: { type: String },
+  cookTime: { type: String, default: "0" },
+  createdAt: {
+    type: String,
+    default: () => moment().format("YYYY-MM-DD HH:mm:ss"),
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
--- models/seed_recipes.js
+++ models/seed_recipes.js
@@ -1,30 +1,21 @@
-
 const seedRecipes = [
-    {
-        title: 'MeatLoaf',
-        ingredients:'1-1/2 pounds Ground Beef (93% lean or leaner), 3/4 cup panko bread crumbs',
-        instructions: 'Preheat oven to 350°F. Combine Ground Beef, bread crumbs, 1/2 cup ketchup, onion, egg, Worcestershire, garlic, thyme, pepper and salt in large bowl, mixing lightly but thoroughly. Shape beef into 8 x 4-inch loaf on rack in aluminum foil-lined broiler pan. Place on upper oven rack in 350°F oven. Bake 40 to 50 minutes; brush with remaining 1/4 cup ketchup. Bake 5 minutes more or until instant-read thermometer inserted into center registers 160°F. Let stand 10 minutes. Cut into slices. ',
-        cookTime: '1hr 15mins',
-    },
-    {
-        title: 'chicken stir-fry ',
-        ingredients:
-        '2 chicken breasts, thinly sliced, 1 red bell pepper, sliced, 1 green bell pepper, sliced, 1 onion, sliced, 2 cloves garlic, minced, 2 tbsp soy sauce, 1 tbsp oyster sauce, 1 tbsp hoisin sauce, 1 tbsp vegetable oil, 1 tsp cornstarch (optional, for thickening), Cooked rice, to serve,',
-        instructions: "To prepare a chicken stir-fry, you'll need boneless, skinless chicken breasts cut into thin strips, red and yellow bell peppers sliced into strips, broccoli florets, thinly sliced carrots and onions, minced garlic, grated ginger, soy sauce, oyster sauce, hoisin sauce, cornstarch, chicken broth, vegetable oil, salt, pepper, chopped green onions for garnish, and optional sesame seeds. Sauté the chicken until cooked through, then set aside. Sauté vegetables until tender-crisp, add garlic and ginger, then stir in sauces mixed with broth and cornstarch until thickened. Return chicken to pan, toss to combine, garnish with green onions and sesame seeds if desired, and serve over rice or noodles for a delicious meal.",
-        cookTime: 'The cook time for chicken stir-fry typically ranges from 15 to 20 minutes, depending on the thickness of the chicken and desired tenderness of the vegetables'
-    }
-]
-
-
-
-
-
-
-
-
-
-
-
-
+  {
+    title: "MeatLoaf",
+    ingredients:
+      "1-1/2 pounds Ground Beef (93% lean or leaner), 3/4 cup panko bread crumbs",
+    instructions:
+      "Preheat oven to 350°F. Combine Ground Beef, bread crumbs, 1/2 cup ketchup, onion, egg, Worcestershire, garlic, thyme, pepper and salt in large bowl, mixing lightly but thoroughly. Shape beef into 8 x 4-inch loaf on rack in aluminum foil-lined broiler pan. Place on upper oven rack in 350°F oven. Bake 40 to 50 minutes; brush with remaining 1/4 cup ketchup. Bake 5 minutes more or until instant-read thermometer inserted into center registers 160°F. Let stand 10 minutes. Cut into slices. ",
+    cookTime: "1hr 15mins",
+  },
+  {
+    title: "chicken stir-fry ",
+    ingredients:
+      "2 chicken breasts, thinly sliced, 1 red bell pepper, sliced, 1 green bell pepper, sliced, 1 onion, sliced, 2 cloves garlic, minced, 2 tbsp soy sauce, 1 tbsp oyster sauce, 1 tbsp hoisin sauce, 1 tbsp vegetable oil, 1 tsp cornstarch (optional, for thickening), Cooked rice, to serve,",
+    instructions:
+      "To prepare a chicken stir-fry, you'll need boneless, skinless chicken breasts cut into thin strips, red and yellow bell peppers sliced into strips, broccoli florets, thinly sliced carrots and onions, minced garlic, grated ginger, soy sauce, oyster sauce, hoisin sauce, cornstarch, chicken broth, vegetable oil, salt, pepper, chopped green onions for garnish, and optional sesame seeds. Sauté the chicken until cooked through, then set aside. Sauté vegetables until tender-crisp, add garlic and ginger, then stir in sauces mixed with broth and cornstarch until thickened. Return chicken to pan, toss to combine, garnish with green onions and sesame seeds if desired, and serve over rice or noodles for a delicious meal.",
+    cookTime:
+      "The cook time for chicken stir-fry typically ranges from 15 to 20 minutes, depending on the thickness of the chicken and desired tenderness of the vegetables",
+  },
+];
 
 module.exports = seedRecipes;
